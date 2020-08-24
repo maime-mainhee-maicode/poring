6597d9aa7fdd2a06efffef2ddca768fb268808ca
Icyscools <meet.ice1@gmail.com>
2020-08-25 03:49:40 +0700

    Clean Poring code

--- src/Poring.java
+++ src/Poring.java
@@ -14,6 +14,7 @@ public class Poring implements Runnable {
 
     public Poring(int index) {
         this.index = index;
+        this.init();
     }
 
     private static ImageIcon getPoringIcon() {
@@ -23,50 +24,71 @@ public class Poring implements Runnable {
         return poringIcon;
     }
 
-    public void run() {
-        game = new GUI().getInstance();
-        fr = new JFrame();
-        image = new JLabel();
-        image.setIcon(getPoringIcon());
-        count = new JLabel();
-        count.setText("" + index);
-        fr.addMouseListener(new MouseAdapter() {
+    public void init() {
+        this.game = GUI.getInstance();
+        this.fr = new JFrame();
+        this.image = new JLabel();
+        this.image.setIcon(getPoringIcon());
+        this.count = new JLabel();
+        this.count.setText("" + index);
+        this.random = new Random();
+
+        this.fr.addMouseListener(new MouseAdapter() {
             public void mousePressed(MouseEvent e) {
                 fr.dispose();
                 game.setScore(game.getScore() + 1);
             }
         });
-        fr.setResizable(false);
-        fr.setDefaultCloseOperation(fr.DO_NOTHING_ON_CLOSE);
-        fr.setLayout(new FlowLayout());
-        fr.getContentPane().add(image);
-        fr.getContentPane().add(count);
-        fr.setVisible(true);
+        this.fr.setResizable(false);
+        this.fr.setDefaultCloseOperation(fr.DO_NOTHING_ON_CLOSE);
+        this.fr.setLayout(new FlowLayout());
+        this.fr.getContentPane().add(image);
+        this.fr.getContentPane().add(count);
+        this.fr.setVisible(true);
+    }
+
+
+    public void run() {
         Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
-        random = new Random();
-        fr.pack();
-        x = random.nextInt((int) dimension.getWidth() - fr.getWidth());
-        y = random.nextInt((int) dimension.getHeight() - fr.getHeight());
-        fr.setLocation(x, y);
-        vibrate();
+        this.random = new Random();
+        this.fr.pack();
+        x = this.random.nextInt((int) dimension.getWidth() - fr.getWidth());
+        y = this.random.nextInt((int) dimension.getHeight() - fr.getHeight());
+        this.fr.setLocation(x, y);
+        this.vibrate();
     }
 
     public void vibrate() {
         try {
             while (true) {
-                Thread.sleep(100);
-                x += random.nextInt(20);
-                y += random.nextInt(20);
-                fr.setLocation(x, y);
-                fr.setLocation(x, y);
-                Thread.sleep(100);
-                x -= random.nextInt(10);
-                y += random.nextInt(20);
-                fr.setLocation(x, y);
+                Thread.sleep(125l);
+                x += this.random.nextInt(20) * this.random.nextDouble() > 0.5 ? 1 : -1;
+                y += this.random.nextInt(20) * this.random.nextDouble() > 0.5 ? 1 : -1;
+                x = setBoundaryX(x);
+                y = setBoundaryY(y);
                 fr.setLocation(x, y);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public boolean isInsideScreen(int x, int y) {
+        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+        boolean left = x >= 0;
+        boolean right = x + this.fr.getWidth() <= dimension.getWidth();
+        boolean top = y >= 0;
+        boolean bottom = y + this.fr.getHeight() <= dimension.getHeight();
+        return left && right && top && bottom;
+    }
+
+    public int setBoundaryX(int x) {
+        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+        return (int) Math.max(0, Math.min(x, dimension.getWidth() - (x + this.fr.getWidth())));
+    }
+
+    public int setBoundaryY(int y) {
+        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+        return (int) Math.max(0, Math.min(y, dimension.getHeight() - (x + this.fr.getHeight())));
     }
 }
